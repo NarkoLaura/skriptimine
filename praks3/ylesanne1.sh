ee03633106e716ddac8d2ac3ef16f0157bce7296
NarkoLaura <laura-lisette.anniko@voco.ee>
2025-09-05 09:55:58 +0300

    Kasutaja käest täisnime küsimine ja tervitamine

--- praks3/ylesanne1.sh
+++ praks3/ylesanne1.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
-# Fail: ylesanne1.sh
-# Autor: Laura-Lisette Anniko
-# Kuupäev: 05.09.2025
 # Kirjeldus: Skript küsib kasutaja ees- ja perekonna nime ning tervitab teda.
 
 echo -n "Sisesta oma ees- ja perenimi: "
